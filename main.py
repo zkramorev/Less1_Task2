ff5bff4d46d4cd0e948c63bdf1e9b67b0902c6a7
zkramorev <legolove001@mail.ru>
2020-11-22 10:30:45 +0300

    second_vers

--- main.py
+++ main.py
@@ -1,9 +1,35 @@
 import sys
 import random
 from PyQt5.QtCore import Qt, QPoint
+from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtGui import QPainter, QColor, QPolygon
 from PyQt5.QtWidgets import QWidget, QApplication, QMainWindow
-from okr import Ui_MainWindow
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(647, 481)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(10, 10, 75, 23))
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 647, 21))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Окружности"))
+        self.pushButton.setText(_translate("MainWindow", "окружность"))
 
 
 class First(QMainWindow, Ui_MainWindow):
@@ -21,7 +47,7 @@ class First(QMainWindow, Ui_MainWindow):
             qp = QPainter()
             qp.begin(self)
             self.f = False
-            qp.setBrush(QColor(255, 255, 0))
+            qp.setBrush(QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
             k = random.randint(20, 320)
             qp.drawEllipse(220, 150, k, k)
             qp.end()
